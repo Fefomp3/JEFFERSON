9ed5f00ab23b0e02ac23bc7dd19773b29c737076
Fefomp3 <felipe.tozzini.teixeira@escola.pr.gov.br>
2025-08-26 21:15:32 +0000

    Add frequency calculation for terms in proctext function

--- script.js
+++ script.js
@@ -16,6 +16,20 @@ campoResultado.textContent = termosChave.join(" - ");
 }
 function proctext(text) {
   let termo = text.split(/\P{L}+/u);
+  
+  let frequency = {};
+
+  for (let i of termo) {
+  frequency[i] = 0;
+
+  for (let j of termo) {
+    if (i == j) {
+      frequency[i]++;
+    }
+  }
+}
+
+console.log(frequency);
 
   return termo;
 }
